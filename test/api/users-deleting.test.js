114a8d268a0bfca5b014afa35e620289df2c5191
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-10-15 12:10:10 +0300

    chore: check if linked stuff is deleted as well

--- test/api/users-deleting.test.js
+++ test/api/users-deleting.test.js
@@ -1,7 +1,7 @@
 const { startServer, stopServer } = require('../../lib/server.js');
 const { request } = require('../scripts/helpers');
 const generator = require('../scripts/generator');
-const { User } = require('../../models');
+const { User, CircleMembership, BodyMembership } = require('../../models');
 
 describe('User deleting', () => {
     beforeAll(async () => {
@@ -71,5 +71,37 @@ describe('User deleting', () => {
         expect(res.body.success).toEqual(false);
         expect(res.body).not.toHaveProperty('data');
         expect(res.body).toHaveProperty('message');
+    });
+
+    test('should delete linked things', async () => {
+        const user = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'delete', object: 'member' });
+
+        const body = await generator.createBody();
+        const circle = await generator.createCircle();
+        await generator.createBodyMembership(body, user);
+        await generator.createCircleMembership(circle, user);
+
+        const res = await request({
+            uri: '/members/' + user.id,
+            method: 'DELETE',
+            headers: { 'X-Auth-Token': token.value }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('message');
+
+        const userFromDb = await User.findByPk(user.id);
+        expect(userFromDb).toEqual(null);
+
+        const bodyMembershipsCount = await BodyMembership.count({ where: { user_id: user.id } });
+        expect(bodyMembershipsCount).toEqual(0);
+
+        const circleMembershipsCount = await CircleMembership.count({ where: { user_id: user.id } });
+        expect(circleMembershipsCount).toEqual(0);
     });
 });
